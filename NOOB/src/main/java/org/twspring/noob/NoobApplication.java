f2a436651cbd9b702aadcce25589c0502c1390f1
Rafeef Alghamdi <115750786+Rie0@users.noreply.github.com>
2024-09-11 13:27:47 +0300

    Add files via upload

---
+++ NOOB/src/main/java/org/twspring/noob/NoobApplication.java
@@ -0,0 +1,13 @@
+package org.twspring.noob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoobApplication.class, args);
+    }
+
+}
