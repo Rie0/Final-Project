112ff22ed6b49a8085ae549e40b3880f41ab43d5
Rafeef Alghamdi <115750786+Rie0@users.noreply.github.com>
2024-09-11 14:35:27 +0300

    DTO(WIP)

---
+++ NOOB/src/main/java/org/twspring/noob/DTO/PlayerDTO.java
@@ -0,0 +1,41 @@
+package org.twspring.noob.DTO;
+
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class PlayerDTO {
+
+    @NotEmpty(message = "Username cannot be empty")
+    @Size(min=4,max = 10,
+            message = "Username must have between 4 to 10 characters")
+    private String username;
+
+    @NotEmpty(message = "Password cannot be empty")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[\\W_]).{6,}$",
+            message = "Password must be strong (at least: at least 6 characters, one uppercase letter, one lowercase letter, one number, and one special character)")
+    private String password;
+
+
+    @NotEmpty(message = "Email cannot be empty")
+    @Email(message = "Email must have a valid format")
+    @Size(min=7,max = 50,
+            message = "Email must have between 7 to 50 letters")
+    private String email;
+
+    @NotBlank(message = "Phone number cannot be blank")
+    @Pattern(regexp = "^\\+9665\\d{8}$",
+            message = "Phone number must be a valid Saudi phone number")
+    private String phoneNumber;
+
+    @CreationTimestamp
+    private final LocalDateTime joinedAt = LocalDateTime.now();
+
+}
--- NOOB/src/main/java/org/twspring/noob/Service/PlayerService.java
+++ NOOB/src/main/java/org/twspring/noob/Service/PlayerService.java
@@ -1,4 +1,23 @@
 package org.twspring.noob.Service;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.twspring.noob.Model.Player;
+import org.twspring.noob.Repository.AuthRepository;
+import org.twspring.noob.Repository.PlayerRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class PlayerService {
+    private final PlayerRepository playerRepository;
+    private final AuthRepository authRepository;
+
+    public List<Player> getPlayers() {
+        return playerRepository.findAll();
+    }
+
+    public void registerPlayer(Player player) {}
 }
